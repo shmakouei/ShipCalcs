45cf2c0a0d2b4df544a556ab136840ce4afa91b2
shmakouei <97775395+shmakouei@users.noreply.github.com>
2024-08-25 23:31:33 -0700

    Add files via upload

---
+++ sidebar.js
@@ -0,0 +1,9 @@
+// Highlight the active link in the sidebar
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll('nav ul li a');
+const menuLength = menuItem.length;
+for (let i = 0; i < menuLength; i++) {
+    if (menuItem[i].href === currentLocation) {
+        menuItem[i].className = 'active';
+    }
+}
